96fd2cead27ce3ed571b4978f5a65e900f7ba39b
Roland <cook.rolandcook.roland@gmail.com>
2023-09-17 21:29:53 -0400

    removed unused imports/ setup navbar

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navbar from "./components/NavBar/Navbar"
+
 
 function App() {
 
   return (
     <>
-      <h1>Portfolio Website</h1>
+      <Navbar/>
     </>
   )
 }
