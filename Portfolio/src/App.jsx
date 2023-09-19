c3c07eaf293faf8f8d6874c8f791cb2e25498c20
Roland <cook.rolandcook.roland@gmail.com>
2023-09-19 13:18:41 -0400

    Created header section

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,12 +1,13 @@
 import Navbar from "./components/NavBar/Navbar"
 import { BrowserRouter } from "react-router-dom"
-
+import Intro from "./components/Intro/intro"
 function App() {
 
   return (
     <>
     <BrowserRouter>
           <Navbar/>
+          <Intro/>
     </BrowserRouter>
     </>
   )
---
+++ Portfolio/src/components/Intro/intro.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./intro.scss"
+import { Link } from "react-router-dom";
+import btnImg from "../../assets/hireme.png"
+
+
+
+
+
+const Intro = () => {
+  return (
+    <section id="intro">
+        <div className="introContent">
+            <span className="hello">Hello,</span>
+            <span className="introText"> I'm <span className="introName"> Roland </span> <br />Full-Stack Developer</span>
+            
+            <p className="introPara">I am a skilled and passionate Full Stack Developer with experience in creating <br /> appealing and user-friendly websites.</p>
+            
+            <Link><button className="btn"><img src={btnImg} alt="" className='btnImg' /> Hire me</button></Link>
+        </div>
+    </section>
+  )
+}
+
+export default Intro
